d3ddde70d46becc71842556ce7494218de298826
Anton <antonzhygalov@gmail.com>
2017-08-22 16:21:05 +0300

    test

---
+++ script2.js
@@ -0,0 +1,19 @@
+// var myHeaders = new Headers();
+// myHeaders.append("Content-Type", "text/plain");
+// myHeaders.append("Content-Length", content.length.toString());
+// var myInit = {method: 'GET',
+// headers: };
+// var someA =  {'https://cors-anywhere.herokuapp.com/'+ mode: 'no-cors'};
+// var header = new Headers({
+//     'Access-Control-Allow-Origin':'*',
+//     'Content-Type': 'text/html'
+// });
+// var options = {method: 'GET', mode: 'no-corse', headers: header};
+fetch('http://marsweather.ingenology.com/v1/archive/?format=json',{'Content-Type':'application/json'}).then(function(response) {
+  return response.json();
+}).then(function(returnedValue) {
+  console.log(returnedValue);
+    // ...
+}).catch(function(err) {
+    console.log(err);
+});
